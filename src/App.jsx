333e6e8149c5e99799c063cbfb28cbc55bd4350d
Mo-Deck <mohamedabow6774@gmail.com>
2024-12-23 09:18:06 +0300

    NetflixClone

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+ 
+  return <h1 className="text-lg font-semibold">Mohamed</h1>
+}
+
+export default App
